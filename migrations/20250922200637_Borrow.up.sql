f2869f8196c0b5b41864d5655526a1ac20f85e2d
kizianov.ao <kizianov.ao@svrauto.ru>
2025-12-10 07:24:28 +0000

    .

--- migrations/20250922200637_Borrow.up.sql
+++ migrations/20250922200637_Borrow.up.sql
@@ -1,8 +1,8 @@
 -- Add up migration script here
 CREATE TABLE IF NOT EXISTS Borrow (
-  id SERIAL PRIMARY KEY,
-  created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-  currency VARCHAR(50),
-  hourly_borrow_rate VARCHAR(50),
-  annualized_borrow_rate VARCHAR(50)
+    id SERIAL PRIMARY KEY,
+    created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
+    currency VARCHAR(50),
+    hourly_borrow_rate VARCHAR(50),
+    annualized_borrow_rate VARCHAR(50)
 );
--- migrations/20251121205503_events_table.up.sql
+++ migrations/20251121205503_events_table.up.sql
@@ -1,9 +1,9 @@
 -- Add up migration script here
 CREATE TABLE IF NOT EXISTS events (
-  id SERIAL PRIMARY KEY,
-  exchange VARCHAR NOT NULL,
-  msg TEXT NOT NULL,
-  created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+    id SERIAL PRIMARY KEY,
+    exchange VARCHAR NOT NULL,
+    msg TEXT NOT NULL,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE INDEX idx_events_exchange ON events(exchange);
