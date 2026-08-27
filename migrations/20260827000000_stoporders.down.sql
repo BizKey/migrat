5b380afb659906017a6bc743af3773e3aa311ff1
BizKey <toonychannel@gmail.com>
2026-08-27 21:56:30 +0300

    .

--- migrations/20260827000000_stoporders.down.sql
+++ migrations/20260827000000_stoporders.down.sql
@@ -1,8 +1,3 @@
-DROP INDEX IF EXISTS idx_stoporders_order_id;
 DROP INDEX IF EXISTS idx_stoporders_updated_at;
-DROP INDEX IF EXISTS idx_stoporders_created_at;
-DROP INDEX IF EXISTS idx_stoporders_exchange_status;
-DROP INDEX IF EXISTS idx_stoporders_exchange_symbol;
-DROP INDEX IF EXISTS idx_stoporders_exchange_client_oid;
 
 DROP TABLE IF EXISTS stoporders;
